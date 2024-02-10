512b55969fb6124e04d584e93fd0423c308c4895
paraslov <paraslovjm@gmail.com>
2024-02-10 09:55:36 +0600

    s1w2 (tech): add posts POST route tests for validations cases

--- src/modules/posts/__tests__/posts.e2e.test.ts
+++ src/modules/posts/__tests__/posts.e2e.test.ts
@@ -2,6 +2,8 @@ import { app, db } from '../../../app/app'
 import { RoutesList } from '../../../app/config/routes'
 import { HttpStatusCode } from '../../common/enums'
 import { testPost } from '../mocks/postsMock'
+import { postsTestManager } from '../utils/testing/postsTestManager'
+import { testBlog } from '../../blogs'
 
 const supertest = require('supertest')
 
@@ -12,12 +14,84 @@ describe('/posts route GET tests: ', () => {
     await request.delete(`${RoutesList.TESTING}/all-data`)
   })
 
-  it('GET /posts', async () => {
+  it('GET /posts success', async () => {
     db.posts = [testPost]
     const result = await request.get(RoutesList.POSTS).expect(HttpStatusCode.OK_200)
 
     expect(result.body?.length).toBe(1)
     expect(result.body[0].blogId).toBe(testPost.blogId)
     expect(result.body[0].title).toBe(testPost.title)
+  })
+})
+
+describe('/posts route POST tests: ', () => {
+  beforeEach(async () => {
+    await request.delete(`${RoutesList.TESTING}/all-data`)
+  })
+  //
+  // it('POST /posts success', async () => {
+  //   await postsTestManager.createPost()
+  // })
+
+  it('POST /posts failed::auth', async () => {
+    await postsTestManager.createPost({
+      user: 'badUser',
+      password: 'badPassword',
+      expectedStatusCode: HttpStatusCode.UNAUTHORIZED_401,
+    })
+    await postsTestManager.createPost({
+      password: 'badPassword',
+      expectedStatusCode: HttpStatusCode.UNAUTHORIZED_401,
+    })
+    await postsTestManager.createPost({
+      user: 'badUser',
+      expectedStatusCode: HttpStatusCode.UNAUTHORIZED_401,
+    })
+  })
+
+  it('POST /posts failed::title', async () => {
+    db.blogs = [testBlog]
+    await postsTestManager.createPost({
+      shouldExpect: true,
+      expectedStatusCode: HttpStatusCode.BAD_REQUEST_400,
+      checkedData: { field: 'title', value: null }
+    })
+    await postsTestManager.createPost({
+      shouldExpect: true,
+      expectedStatusCode: HttpStatusCode.BAD_REQUEST_400,
+      checkedData: { field: 'title', value: '1234567890123456789012345678901' }
+    })
+  })
+
+  it('POST /posts failed::shortDescription', async () => {
+    db.blogs = [testBlog]
+    await postsTestManager.createPost({
+      shouldExpect: true,
+      expectedStatusCode: HttpStatusCode.BAD_REQUEST_400,
+      checkedData: { field: 'shortDescription', value: '' }
+    })
+  })
+
+  it('POST /posts failed::content', async () => {
+    db.blogs = [testBlog]
+    await postsTestManager.createPost({
+      shouldExpect: true,
+      expectedStatusCode: HttpStatusCode.BAD_REQUEST_400,
+      checkedData: { field: 'content', value: 123 }
+    })
+  })
+
+  it('POST /posts failed::blogId', async () => {
+    db.blogs = [testBlog]
+    await postsTestManager.createPost({
+      shouldExpect: true,
+      expectedStatusCode: HttpStatusCode.BAD_REQUEST_400,
+      checkedData: { field: 'blogId', value: '182736' }
+    })
+    await postsTestManager.createPost({
+      shouldExpect: true,
+      expectedStatusCode: HttpStatusCode.BAD_REQUEST_400,
+      checkedData: { field: 'blogId', value: null }
+    })
   })
 })
---
+++ src/modules/posts/utils/testing/postsTestManager.ts
@@ -0,0 +1,50 @@
+import { RoutesList } from '../../../../app/config/routes'
+import { HttpStatusCode } from '../../../common/enums'
+import { app, db } from '../../../../app/app'
+import { testPostInput } from '../../mocks/postsMock'
+
+const supertest = require('supertest')
+
+const request = supertest(app)
+
+class PostsTestManager {
+  async createPost(payload: {
+    shouldExpect?: boolean
+    user?: string
+    password?: string
+    expectedStatusCode?: HttpStatusCode
+    checkedData?: { field: string, value: any }
+  } = {}) {
+    const {
+      shouldExpect = false,
+      user = 'admin',
+      password = 'qwerty',
+      expectedStatusCode = HttpStatusCode.CREATED_201,
+      checkedData,
+    } = payload
+
+    const result = await request.post(RoutesList.POSTS)
+      .auth(user, password)
+      .send(checkedData ? { ...testPostInput, [checkedData.field]: checkedData.value } : testPostInput)
+      .expect(expectedStatusCode)
+
+    if (shouldExpect && expectedStatusCode === HttpStatusCode.CREATED_201) {
+      expect(result.body.title).toBe(testPostInput.title)
+      expect(result.body.blogId).toBe(testPostInput.blogId)
+      expect(db.posts[0].shortDescription).toStrictEqual(testPostInput.shortDescription)
+      expect(db.posts[0].id).toStrictEqual(expect.any(String))
+    }
+
+    if (shouldExpect && expectedStatusCode === HttpStatusCode.BAD_REQUEST_400 && checkedData?.field) {
+      console.log('@> errors: ', result.body.errorsMessages)
+      expect(result.body.errorsMessages.length).toBe(1)
+      expect(result.body.errorsMessages[0].field).toBe(checkedData.field)
+      expect(result.body.errorsMessages[0].message).toStrictEqual(expect.any(String))
+      expect(db.posts.length).toBe(0)
+    }
+
+    return result
+  }
+}
+
+export const postsTestManager = new PostsTestManager()
