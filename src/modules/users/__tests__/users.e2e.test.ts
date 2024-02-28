5983a324101dd0490939a9701412e8ccbb0f70e0
paraslov <paraslovjm@gmail.com>
2024-02-28 21:28:54 +0600

    s2w1 (feat): add more cases for users tests

--- src/modules/users/__tests__/users.e2e.test.ts
+++ src/modules/users/__tests__/users.e2e.test.ts
@@ -27,10 +27,48 @@ describe('/users route GET tests: ', () => {
       .expect(HttpStatusCode.OK_200)
 
     expect(result.body.items?.length).toBe(1)
-    expect(result.body.items[0].blogId).toBe(createdUser.body.blogId)
-    expect(result.body.items[0].title).toBe(createdUser.body.title)
+    expect(result.body.items[0].login).toBe(createdUser.body.login)
+    expect(result.body.items[0].email).toBe(createdUser.body.email)
     expect(result.body.totalCount).toBe(1)
     expect(result.body.pageSize).toBe(10)
+  })
+
+  it('GET /users success email search', async () => {
+    const createdUser = await usersTestManager.createUser()
+    const result = await request.get(RoutesList.USERS)
+      .auth('admin', 'qwerty')
+      .query({
+        searchEmailTerm: 'mye',
+        searchLoginTerm: '123',
+      })
+      .expect(HttpStatusCode.OK_200)
+
+    expect(result.body.items?.length).toBe(1)
+    expect(result.body.items[0].login).toBe(createdUser.body.login)
+    expect(result.body.items[0].email).toBe(createdUser.body.email)
+    expect(result.body.totalCount).toBe(1)
+    expect(result.body.pageSize).toBe(10)
+  })
+
+  it('GET /users success (200) login search with no results', async () => {
+    const createdUser = await usersTestManager.createUser()
+    const result = await request.get(RoutesList.USERS)
+      .auth('admin', 'qwerty')
+      .query({
+        searchEmailTerm: 'yololo',
+        searchLoginTerm: '123',
+      })
+      .expect(HttpStatusCode.OK_200)
+
+    expect(result.body.items?.length).toBe(0)
+    expect(result.body.totalCount).toBe(0)
+    expect(result.body.pageSize).toBe(10)
+  })
+
+  it('GET /users failed::unathorized', async () => {
+    await usersTestManager.createUser()
+    await request.get(RoutesList.USERS)
+      .expect(HttpStatusCode.UNAUTHORIZED_401)
   })
 })
 
@@ -67,7 +105,6 @@ describe('/users route POST tests: ', () => {
       checkedData: { field: 'email', value: 'sameEmail@gmail.com' },
       expectedStatusCode: HttpStatusCode.BAD_REQUEST_400,
     })
-    console.log('@> result: ', result.body)
 
     expect(result.body.errorsMessages.find((err: any) => err.field === 'email').message)
       .toBe('This email is already exists')
