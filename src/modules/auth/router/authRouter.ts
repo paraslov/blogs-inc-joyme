8abee07e1a119af577c17456fd0c8a6f8ff97ce5
paraslov <paraslovjm@gmail.com>
2024-02-29 21:27:14 +0600

    s2w1 (feat): add auth router

--- src/modules/auth/router/authRouter.ts
+++ src/modules/auth/router/authRouter.ts
@@ -1,4 +1,18 @@
-import { Router } from 'express'
+import { Router, Response } from 'express'
+import { RequestBody } from '../../common/types'
+import { AuthInputModel } from '../model/types/AuthInputModel'
+import { authService } from '../model/services/authService'
+import { HttpStatusCode } from '../../common/enums'
+import { authPostValidation } from '../validations/authValidations'
 
 export const authRouter = Router()
 
+authRouter.post('/login', authPostValidation(), async (req: RequestBody<AuthInputModel>, res: Response) => {
+  const isAuthPassed = await authService.checkUser(req.body.loginOrEmail, req.body.password)
+
+  if (!isAuthPassed) {
+    return res.sendStatus(HttpStatusCode.UNAUTHORIZED_401)
+  }
+
+  return res.sendStatus(HttpStatusCode.NO_CONTENT_204)
+})
