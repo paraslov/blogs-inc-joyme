ae203f2bedee00a811850c3080c09899fd6bcc99
paraslov <paraslovjm@gmail.com>
2024-03-05 20:31:35 +0500

    s2w2 (feat): update jwt service. Add env secret for jwt

--- src/app/appSettings.ts
+++ src/app/appSettings.ts
@@ -1,3 +1,4 @@
 export const AppSettings = {
   PORT: 3003,
+  JWT_SECRET: process.env.JWT_SECRET,
 }
---
+++ src/modules/common/services/jwtService.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+import { UserDbModel } from '../../users'
+import { ObjectId, WithId } from 'mongodb'
+import { AppSettings } from '../../../app/appSettings'
+
+export const jwtService = {
+  async createJWT(user: WithId<UserDbModel>) {
+    if (!AppSettings.JWT_SECRET) {
+      return false
+    }
+    return jwt.sign({ userId: user._id }, AppSettings.JWT_SECRET, { expiresIn: '1h' })
+  },
+  async getUserIdByToken(token: string) {
+    if (!AppSettings.JWT_SECRET) {
+      return null
+    }
+
+    try {
+      const res: any = jwt.verify(token, AppSettings.JWT_SECRET)
+
+      return res.userId
+    } catch (err) {
+      return null
+    }
+  }
+}
