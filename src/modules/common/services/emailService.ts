f71fc9a7119caeff4b336548b17fe9f60834267e
paraslov <paraslovjm@gmail.com>
2024-03-16 19:36:20 +0500

    Revert "s2w3 (feat): fix transport"
    This reverts commit 4c0f476e5550feed36e3565d63586a851e72a457.

--- src/modules/common/services/emailService.ts
+++ src/modules/common/services/emailService.ts
@@ -12,7 +12,6 @@ const transporter = nodemailer.createTransport({
 
 export const emailService = {
   async sendEmail(emailTemplate: MailOptions) {
-    console.log('@> transport: ', transporter)
     const info = await transporter.sendMail(emailTemplate)
 
     return info
