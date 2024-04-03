9a269a41ca09a2703a35d76674972321d29ee792
paraslov <paraslovjm@gmail.com>
2024-04-03 19:38:36 +0500

    s3w1 (feat): add delete sessions service

--- src/modules/devices/model/services/devicesService.ts
+++ src/modules/devices/model/services/devicesService.ts
@@ -1,5 +1,6 @@
 import { jwtService } from '../../../common/services'
 import { devicesQueryRepository } from '../repositories/devicesQueryRepository'
+import { devicesCommandRepository } from '../repositories/devicesCommandRepository'
 
 export const devicesService = {
   async checkAuthSessionByRefreshToken(userId: string, refreshToken: string) {
@@ -8,10 +9,21 @@ export const devicesService = {
       return false
     }
 
-    const hasAuthSession = await devicesQueryRepository.isAuthSessionExist(userId, tokenData.deviceId, tokenData?.iat)
-    return hasAuthSession;
+    const authSession = await devicesQueryRepository.isAuthSessionExist(userId, tokenData.deviceId, tokenData?.iat)
+    return authSession;
+  },
+  async deleteAllOtherSessions(userId: string, currentDeviceId: string) {
+    const userDevices = await devicesQueryRepository.getDevices(userId)
+    const userDevicesIdsWithoutCurrent = userDevices
+      .map((deviceData) => deviceData.deviceId)
+      .filter((deviceId) => deviceId !== currentDeviceId)
+
+    return await devicesCommandRepository.deleteAllOtherSessions(userDevicesIdsWithoutCurrent)
+  },
+  async deleteSessionByDeviceId(deviceId: string) {
+    return devicesCommandRepository.deleteSessionByDeviceId(deviceId)
   },
   async getTokenData(refreshToken: string) {
-    return jwtService.decodeToken(refreshToken)
+    return jwtService.getDataByTokenAndVerify(refreshToken, 'refresh')
   },
 }
