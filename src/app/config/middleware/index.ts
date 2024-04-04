cd876d0a7c605c18475e73ae18ca2ca371f08f2b
paraslov <paraslovjm@gmail.com>
2024-04-04 18:58:03 +0500

    s3w1 (feat): add rate limit middleware

--- src/app/config/middleware/index.ts
+++ src/app/config/middleware/index.ts
@@ -2,10 +2,12 @@ import { authMiddleware } from './authMiddleware'
 import { jwtAuthMiddleware } from './jwtAuthMiddleware'
 import { inputValidationMiddleware } from './inputValidationMiddleware'
 import { sortingAndPaginationMiddleware } from './sortingAndPaginationMiddleware'
+import { rateLimitMiddleware } from './rateLimitMiddleware'
 
 export {
   authMiddleware,
   jwtAuthMiddleware,
   inputValidationMiddleware,
   sortingAndPaginationMiddleware,
+  rateLimitMiddleware,
 }
