88afb1c7a2917acaf254fdc942b826ac23a49e7a
paraslov <paraslovjm@gmail.com>
2024-04-10 20:35:14 +0500

    s3w2 (feat): add password recovery to email services

--- src/modules/common/services/mailTemplatesService.ts
+++ src/modules/common/services/mailTemplatesService.ts
@@ -21,4 +21,19 @@ export const mailTemplatesService = {
              </p>`,
     };
   },
+  getPasswordRecoveryMailTemplate(userEmail: string, confirmationCode: string) {
+    return {
+      from: '"JoyMe Studios" <joymestudios@gmail.com>',
+      to: userEmail,
+      subject: 'Password recovery for "Blogs inc. JoyMe"',
+      html: `<h1>Recovery your password on "Blogs inc. JoyMe"</h1>
+             <p>To finish password recovery please follow the link:
+                 <a href='https://google.com?code=${confirmationCode}'>complete registration</a>
+             </p>
+             <p>
+                If you have not registreted at "Blogs inc. JoyMe" just ignore this message
+             </p>
+`,
+    };
+  },
 }
